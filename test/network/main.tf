3878b52ced291917e1f90488e43f74b09700a5cd
Tyler Tidman <tyler.tidman@draak.ca>
2017-07-18 14:26:47 -0400

    Rename layer in environment test

--- test/network/main.tf
+++ test/network/main.tf
@@ -12,9 +12,9 @@ Terraform Remote State and Locking
 
 terraform {
   backend "s3" {
-    bucket  = "cace1-tf-orthos-test"       # ${var.state_bucket_name}
-    key     = "test/vpc/terraform.tfstate"
-    region  = "ca-central-1"               # ${var.region}
+    bucket  = "cace1-tf-orthos-test"           # ${var.state_bucket_name}
+    key     = "test/network/terraform.tfstate"
+    region  = "ca-central-1"                   # ${var.region}
     encrypt = true
     acl     = "private"
 
