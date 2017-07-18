37db550573c08e9f0c82d2f904783c7de47292d0
Tyler Tidman <tyler.tidman@draak.ca>
2017-07-18 13:59:26 -0400

    Made the state bucket name less likely to clash

--- global/main.tf
+++ global/main.tf
@@ -12,7 +12,7 @@ Terraform Remote State and Locking
 
 terraform {
   backend "s3" {
-    bucket         = "cace1-tf-test"            # ${var.state_bucket_name}
+    bucket         = "cace1-tf-orthos-test"     # ${var.state_bucket_name}
     key            = "global/terraform.tfstate"
     region         = "ca-central-1"             # ${var.region}
     encrypt        = true
--- global/variables.tf
+++ global/variables.tf
@@ -9,5 +9,5 @@ variable "region" {
 
 variable "state_bucket_name" {
   description = "Name of Terraform state bucket"
-  default     = "cace1-tf-test"
+  default     = "cace1-tf-orthos-test"
 }
